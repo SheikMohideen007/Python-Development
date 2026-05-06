5c7441dadc85cc62ebff1c7b6a67282be6754cb3
SheikMohideen007 <sheikmohideen20010@gmail.com>
2026-05-06 16:20:56 +0530

    docs : print types

---
+++ strong fundamentals/printing.py
@@ -0,0 +1,13 @@
+greet="Hello Bro !!"
+print(greet)
+print(str(greet))
+print(repr(greet))
+animals = 'tiger'
+print(f'My hovercraft is full of {animals}.')
+print('We are the {} who say "{}!"'.format('knights', 'Hi'))
+
+# Output : Hello Bro !!
+# Hello Bro !!
+# 'Hello Bro !!'
+# My hovercraft is full of tiger.
+# We are the knights who say "Hi!"
