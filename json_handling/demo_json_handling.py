264cec036b4197ebcaad22a46b75e410df20546b
SheikMohideen007 <sheikmohideen20010@gmail.com>
2026-05-06 16:21:39 +0530

    refactor : json load and dump code

--- json_handling/demo_json_handling.py
+++ json_handling/demo_json_handling.py
@@ -14,8 +14,8 @@ with open('response.json', 'r') as file:
 print(data)
 
 # Converting Python dictionary to JSON string and writing to a file
-python_dict={'username':'Sheik','age':24,'is_dev':True,'Hobbies':['Coding','Reading','Traveling']}
-json_string=json.dumps(python_dict,indent=4)
+python_data['Hobbies']=['Coding','Traveling','Cooking','Singing']
+json_string=json.dumps(python_data,indent=4)
 with open("output.json","w") as file:
     file.write(json_string)
     file.close()
